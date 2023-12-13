d1afb47fc9ad2dc9f1ac973ec6e0d9edeca356a5
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-13 09:43:56 -0500

    final product

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    num1 = 0
+    num2 = 1
+    count = 0
+    fib_sequence = []
+
+    while count < length:
+        fib_sequence.append(num1)
+        num1, num2 = num2, num1 + num2
+        count += 1
+
+    print(f'[{", ".join(map(str, fib_sequence))}]')
+
+print(print_fibonacci(9))
